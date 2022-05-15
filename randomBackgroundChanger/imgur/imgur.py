3e769af5c93af14be7e383a462e4763d42a8c112
Jack Dane <jackd98.jd@gmail.com>
2022-05-15 22:35:02 +0100

    #1 - Only accept images
    * Don't allow videos to be saved for use.

--- randomBackgroundChanger/imgur/imgur.py
+++ randomBackgroundChanger/imgur/imgur.py
@@ -26,13 +26,22 @@ class ImgurController:
         for imageResponse in responseData["data"]:
             title = imageResponse["title"]
             if imageResponse.get("images"):
+                # Just use the first image
+                imageFileFormat = imageResponse["images"][0]["type"]
                 imageLink = imageResponse["images"][0]["link"]
                 imageType = imageResponse["images"][0]["type"]
             else:
+                imageFileFormat = imageResponse["type"]
                 imageLink = imageResponse["link"]
                 imageType = imageResponse["type"]
-            imageURLs.append(ImgurImage(title, imageLink, imageType))
+            if not ImgurController._imageIsVideo(imageFileFormat):
+                # only add images, not videos
+                imageURLs.append(ImgurImage(title, imageLink, imageType))
         return imageURLs
+
+    @staticmethod
+    def _imageIsVideo(imageFileFormat):
+        return "video" in imageFileFormat
 
     def refreshToken(self):
         pass
