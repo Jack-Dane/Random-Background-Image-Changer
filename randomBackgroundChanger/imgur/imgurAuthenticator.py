507d42646b322a411e42db4c4a6eece7bfb913fe
Jack Dane <jackd98.jd@gmail.com>
2022-05-21 11:07:55 +0100

    Saving access token & refresh token to disk
    * Saving the refresh token and access token to a json file on the machine.
    * Using the access token from the JSON file if available instead of requesting another pin for authentication.

--- randomBackgroundChanger/imgur/imgurAuthenticator.py
+++ randomBackgroundChanger/imgur/imgurAuthenticator.py
@@ -1,6 +1,7 @@
 
 import requests
 import webbrowser
+import json
 
 
 class ImgurAuthenticator:
@@ -20,15 +21,41 @@ class ImgurAuthenticator:
     def pinAuthenticationURL(self):
         return f"https://api.imgur.com/oauth2/authorize?client_id={self.clientId}&response_type=pin"
 
-    def startAuthentication(self):
-        webbrowser.open(self.pinAuthenticationURL)
-        self.pin = input("Enter Pin: ")
-        self.getAccessToken()
-
-    def getAccessToken(self):
-        postData = {
+    @property
+    def authPostData(self):
+        return {
             "client_id": self.clientId,
-            "client_secret": self.clientSecret,
+            "client_secret": self.clientSecret
+        }
+
+    @property
+    def _accessTokens(self):
+        return {
+            "access_token": self.accessToken,
+            "refresh_token": self.refreshToken
+        }
+
+    def _saveTokensToDisk(self):
+        with open("creds.json", "w") as credsFile:
+            json.dump(self._accessTokens, credsFile)
+
+    def startAuthentication(self):
+        try:
+            with open("creds123.json", "r") as credsFile:
+                creds = json.load(credsFile)
+                self.accessToken = creds["access_token"]
+                self.refreshToken = creds["refresh_token"]
+        except FileNotFoundError:
+            self._pinBasedAuthentication()
+
+    def _pinBasedAuthentication(self):
+        webbrowser.open(self.pinAuthenticationURL)
+        self.pin = input("Enter Imgur Pin: ")
+        self._getAccessTokenFromPin()
+
+    def _getAccessTokenFromPin(self):
+        postData = {
+            **self.authPostData,
             "grant_type": "pin",
             "pin": self.pin
         }
@@ -37,3 +64,4 @@ class ImgurAuthenticator:
         response = response.json()
         self.accessToken = response["access_token"]
         self.refreshToken = response["refresh_token"]
+        self._saveTokensToDisk()
