2a02e1e26126edd7558080586007bd44c1c164b6
Jack Dane <jackd98.jd@gmail.com>
2022-05-28 16:25:02 +0100

    FileHandler Abstraction
    * Abstracting the FileHandler GSettings class to enable easier support for other operating systems

--- randomBackgroundChanger/fileHandler/fileHandler.py
+++ randomBackgroundChanger/fileHandler/fileHandler.py
@@ -1,62 +1,30 @@
 
 import json
-from mimetypes import MimeTypes
-import mimetypes
 import os
 import subprocess
 import shutil
 import requests
 from flask import Flask, Response
+from abc import ABC, abstractmethod
 
 PORT = 5000
 
 
-class FileHandler(Flask):
+class FileHandler:
 
     def __init__(self, imgurController, *args, **kwargs):
-        super().__init__(__name__, *args, **kwargs)
         self._imageFilePaths = []
         self._currentImagePath = None
         self._imageController = imgurController
-        self.addExistingImagesToList()
+        self._addExistingImagesToList()
 
-        self.add_url_rule("/", view_func=self.homePage, methods=["GET"])
-
-        self.add_url_rule("/change-background", view_func=self.changeBackground, methods=["POST", "GET"])
-
-        self.add_url_rule("/background-images", view_func=self.backgroundImages, methods=["GET"])
-
-        self.add_url_rule("/current-image", view_func=self.currentImage, methods=["GET"])
-
-    def homePage(self):
-        return Response(status=200)
-
-    def changeBackground(self):
+    def _cycleBackgroundImage(self):
         if not self._imageFilePaths:
             self._getImages()
 
-        # TODO: make this available for other operating systems
         nextImagePath = self._imageFilePaths.pop(0)
-        subprocess.run(
-            ["/usr/bin/gsettings", "set", "org.gnome.desktop.background", "picture-uri", nextImagePath]
-        )
-        subprocess.run(
-            ["/usr/bin/gsettings", "set", "org.gnome.desktop.background", "picture-options", "scaled"]
-        )
-        # TODO: check security considerations for the running this command
         self._deleteLastImage()
         self._currentImagePath = nextImagePath
-        return Response(status=200)
-
-    def backgroundImages(self):
-        response = Response(json.dumps(self._imageFilePaths), mimetype="json")
-        response.headers.add("Access-Control-Allow-Origin", "*")
-        return response
-
-    def currentImage(self):
-        response = Response(json.dumps(self._currentImagePath), mimetype="json")
-        response.headers.add("Access-Control-Allow-Origin", "*")
-        return response
 
     def _getImages(self):
         imgurImages = self._imageController.requestNewImages()
@@ -76,13 +44,21 @@ class FileHandler(Flask):
 
         try:
             os.remove(self._currentImagePath)
-        except Exception:
+        except Exception as e:
             print("Could not delete file")
+            print(str(e))
 
     def getFilePath(self, fileName):
         return os.path.join(self.directoryPath, fileName)
 
-    def addExistingImagesToList(self):
+    def _checkCurrentImageNotInFilePaths(self):
+        if self._currentImagePath in self._imageFilePaths:
+            self._imageFilePaths.remove(self._currentImagePath)
+        else:
+            # We don't want to delete a file the user has added only the random ones
+            self._currentImagePath = None
+
+    def _addExistingImagesToList(self):
         """ Add any images to the path that already exist
         """
         imageFilePaths = list(map(self.getFilePath, os.listdir(self.directoryPath)))
@@ -93,6 +69,76 @@ class FileHandler(Flask):
         return "/home/jack/Documents/Python/autoDesktopChanger/backgroundImages"
 
 
+class HTTPFileHandler(FileHandler, Flask):
+
+    def __init__(self, imgurController, *args, **kwargs):
+        FileHandler.__init__(self, imgurController, *args, **kwargs)
+        Flask.__init__(self, __name__, *args, **kwargs)
+
+        self.add_url_rule("/", view_func=self.homePage, methods=["GET"])
+
+        self.add_url_rule("/change-background", view_func=self.changeBackground, methods=["POST", "GET"])
+
+        self.add_url_rule("/background-images", view_func=self.backgroundImages, methods=["GET"])
+
+        self.add_url_rule("/current-image", view_func=self.currentImage, methods=["GET"])
+
+    def homePage(self):
+        return Response(status=200)
+
+    def changeBackground(self):
+        self._cycleBackgroundImage()
+        return Response(status=200)
+
+    def backgroundImages(self):
+        response = Response(json.dumps(self._imageFilePaths), mimetype="json")
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
+
+    def currentImage(self):
+        response = Response(json.dumps(self._currentImagePath), mimetype="json")
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
+
+
+class BackgroundChangerBase(ABC, HTTPFileHandler):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setCurrentBackgroundImagePath()
+        self._checkCurrentImageNotInFilePaths()
+
+    def _cycleBackgroundImage(self):
+        super()._cycleBackgroundImage()
+        self.changeBackgroundImage()
+
+    @abstractmethod
+    def setCurrentBackgroundImagePath(self):
+        pass
+
+    @abstractmethod
+    def changeBackgroundImage(self):
+        pass
+
+
+class GSettingsHTTPBackgroundChanger(BackgroundChangerBase):
+
+    def setCurrentBackgroundImagePath(self):
+        backgroundPathProcess = subprocess.run(
+            ["/usr/bin/gsettings", "get", "org.gnome.desktop.background", "picture-uri"],
+            capture_output=True
+        )
+        self._currentImagePath = backgroundPathProcess.stdout.decode().split("'")[1]
+
+    def changeBackgroundImage(self):
+        subprocess.run(
+            ["/usr/bin/gsettings", "set", "org.gnome.desktop.background", "picture-uri", self._currentImagePath]
+        )
+        subprocess.run(
+            ["/usr/bin/gsettings", "set", "org.gnome.desktop.background", "picture-options", "scaled"]
+        )
+
+
 def startFileHandlerServer(imageController):
-    fileHandler = FileHandler(imageController)
+    fileHandler = GSettingsHTTPBackgroundChanger(imageController)
     fileHandler.run(port=PORT)
