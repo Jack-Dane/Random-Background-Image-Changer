9f8479ec5c68ff1afafb2308956d7a123803c16b
Jack Dane <jackd98.jd@gmail.com>
2022-07-30 16:12:12 +0100

    CLI for updating Imgur Pin
    * Adding a new CLI command to allow the parsing of the pin to the fileHandler server.
    * Refactoring the parsers for all CLI commands to accept --clientId and --clientSecret.
    * Implementing a token authentication to send the pin and change background image to the fileHandler.

--- randomBackgroundChanger/fileHandler/fileHandlerClient.py
+++ randomBackgroundChanger/fileHandler/fileHandlerClient.py
@@ -1,21 +1,54 @@
+from contextlib import contextmanager
 
 import requests
 
 
 class FileHandlerClient:
 
-    def __init__(self, port):
-        self.port = port
+    def __init__(self, port, clientId, clientSecret):
+        self._port = port
+        self._clientId = clientId
+        self._clientSecret = clientSecret
+        self._token = None
+
+    @contextmanager
+    def _withAuth(self):
+        """ Create temporary token
+        """
+        try:
+            tokenRequest = requests.post(
+                f"http://localhost:{self._port}/token",
+                json={
+                    "clientId": self._clientId,
+                    "clientSecret": self._clientSecret
+                }
+            )
+            self._token = tokenRequest.json().get("token")
+            yield
+        finally:
+            requests.delete(
+                f"http://localhost:{self._port}/token",
+                json={
+                    "clientId": self._clientId,
+                    "clientSecret": self._clientSecret,
+                    "token": self._token
+                }
+            )
+
+    def _sendRequest(self, url, json=None):
+        json = json if json else {}
+        with self._withAuth():
+            response = requests.post(
+                url=url, json=json, headers={"Authorization": f"Bearer: {self._token}"}
+            )
+            response.raise_for_status()
 
     def nextBackgroundImage(self):
-        changeBackgroundRequest = requests.post(
-            f"http://localhost:{self.port}/change-background"
+        self._sendRequest(
+            f"http://localhost:{self._port}/change-background"
         )
-        changeBackgroundRequest.raise_for_status()
 
     def addPin(self, pin):
-        pinRequest = requests.post(
-            f"http://localhost:{self.port}/imgur-pin",
-            json={"pin": pin}
+        self._sendRequest(
+            f"http://localhost:{self._port}/imgur-pin", json={"pin": pin}
         )
-        pinRequest.raise_for_status()
--- randomBackgroundChanger/scripts.py
+++ randomBackgroundChanger/scripts.py
@@ -11,12 +11,45 @@ from randomBackgroundChanger.imgur.imgur import ImgurController
 from randomBackgroundChanger.imgur.imgurAuthenticator import ImgurAuthenticator
 
 
-class StartFilerServer(ABC):
+class BasicArgsParser(ABC):
 
     def __init__(self):
+        self._args = None
+
+    @property
+    @abstractmethod
+    def Parser(self):
+        pass
+
+    @abstractmethod
+    def createInstance(self):
+        pass
+
+    def parseArguments(self, *args):
+        self.Parser.add_argument(
+            "--clientId", required=True,
+            help="The client ID that can be found in the settings of your Imgur account"
+        )
+        self.Parser.add_argument(
+            "--clientSecret", required=True,
+            help="The client secret that can be found in the settings of your Imgur account"
+        )
+        self._args = self.Parser.parse_args(*args)
+        self.createInstance()
+
+
+class StartFilerServer(BasicArgsParser, ABC):
+
+    def __init__(self):
+        super().__init__()
         self._server = None
         self._imgurController = None
         self._imgurAuthenticator = None
+        self._parser = argparse.ArgumentParser(description="Start File Handler")
+
+    @property
+    def Parser(self):
+        return self._parser
 
     @abstractmethod
     def startFileHandler(self):
@@ -26,21 +59,11 @@ class StartFilerServer(ABC):
     def getArgs(self, parser, *args):
         pass
 
-    def parseArguments(self, *args):
-        parser = argparse.ArgumentParser(description="Start File Handler")
-        parser.add_argument(
-            "--clientId", required=True,
-            help="The client ID that can be found in the settings of your Imgur account"
-        )
-        parser.add_argument(
-            "--clientSecret", required=True,
-            help="The client secret that can be found in the settings of your Imgur account"
-        )
-        args = self.getArgs(parser, *args)
-        self._imgurAuthenticator = ImgurAuthenticator(args.clientId, args.clientSecret)
+    def createInstance(self):
+        self._imgurAuthenticator = ImgurAuthenticator(self._args.clientId, self._args.clientSecret)
         self._imgurController = ImgurController(self._imgurAuthenticator)
         self._server = GSettingsHTTPBackgroundChanger(
-            self._imgurController, args.clientId, args.clientSecret
+            self._imgurController, self._args.clientId, self._args.clientSecret
         )
 
 
@@ -74,18 +97,63 @@ def startDevelopmentServer():
     developmentServer.startFileHandler()
 
 
+class FileHandlerClientParser(BasicArgsParser, ABC):
+
+    def __init__(self):
+        super().__init__()
+        self.client = None
+
+    def startClient(self, *args):
+        self.parseArguments(*args)
+        self.createInstance()
+
+    def createInstance(self):
+        self.client = FileHandlerClient(
+            PORT, self._args.clientId, self._args.clientSecret
+        )
+
+
+class FileHandlerClientChangeBackgroundParser(FileHandlerClientParser):
+
+    def __init__(self):
+        super().__init__()
+        self._parser = argparse.ArgumentParser(description="Update Background Image")
+
+    @property
+    def Parser(self):
+        return self._parser
+
+
+class FileHandlerClientAddPinParser(FileHandlerClientParser):
+
+    def __init__(self):
+        super().__init__()
+        self.pin = None
+        self._parser = argparse.ArgumentParser(description="Set Imgur Auth Pin")
+
+    def createInstance(self):
+        super().createInstance()
+        self.pin = self._args.pin
+
+    @property
+    def Parser(self):
+        return self._parser
+
+    def parseArguments(self, *args):
+        self.Parser.add_argument(
+            "--pin", required=True,
+            help="The pin used to authorise the Imgur account"
+        )
+        super().parseArguments(*args)
+
+
 def updateBackgroundImage():
-    client = FileHandlerClient(PORT)
-    client.nextBackgroundImage()
+    parser = FileHandlerClientChangeBackgroundParser()
+    parser.startClient(sys.argv[1:])
+    parser.client.nextBackgroundImage()
 
 
 def addImgurPin():
-    parser = argparse.ArgumentParser(description="Set Imgur Auth Pin")
-    parser.add_argument(
-        "--pin", required=True,
-        help="The pin used to authorise the Imgur account"
-    )
-    args = parser.parse_args()
-
-    client = FileHandlerClient(PORT)
-    client.addPin(args.pin)
+    parser = FileHandlerClientAddPinParser()
+    parser.startClient(sys.argv[1:])
+    parser.client.addPin(parser.pin)
