4e832dbb31a30bd1c8e118d6ec67e7d3a008b279
Jack Dane <jackd98.jd@gmail.com>
2023-01-24 22:27:53 +0000

    Fix ChangeBackground fetch request
    * Check the token is valid and if not request a new one.

--- views/src/components/ChangeBackground.vue
+++ views/src/components/ChangeBackground.vue
@@ -14,20 +14,27 @@ export default {
     methods: {
         changeBackground() {
             let self = this;
-            try {
-                fetch(
-                    "http://localhost:5000/change-background",
-                    {
-                        headers: {
-                            "Content-Type": "application/json",
-                            "Authorization": "Bearer " + self.requests.authorisationToken
-                        }
+            fetch(
+                "http://localhost:5000/change-background",
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": "Bearer " + self.requests.authorisationToken
                     }
-                );
-                self.requests.passedAuth();
-            } catch(error) {
-                self.requests.handleAuthErrors(error);
-            }
+                }
+            ).then(function(response) {
+                if (response.status == 200) {
+                    return
+                }
+                throw new Error(response.status);
+            }).catch(status){
+                if (error.message == 401) {
+                    console.log("Unauthorised, trying to get a new token");
+                    self.requests.setNewToken();
+                    return;
+                }
+                console.error(error);
+            };
         },
     }
 }
